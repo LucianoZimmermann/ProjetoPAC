49e7ba49d3fb3aeb047593c96d211867e1d4dd0d
matheus.lorenzetti <matheuslorenzetti2005@gmail.com>
2024-06-27 20:18:26 -0300

    alterando banco de dados, criando atributo delete

--- Database.py
+++ Database.py
@@ -1,13 +1,14 @@
+import streamlit as st
 import pymysql
 from pymysql import Error
-import streamlit as st
+
 
 def get_connection():
     try:
         connection = pymysql.connect(
-            host='fujama.c1auuo444yeh.us-east-1.rds.amazonaws.com',
+            host='localhost',
             user='root',
-            passwd='fujama1234',
+            passwd='',
             database='fujama'
         )
         if connection:
@@ -16,6 +17,8 @@ def get_connection():
     except Error as e:
         print(f"Erro ao conectar com o banco de dados: {e}")
         return None
+
+
 def create_all_tables(connection):
     if connection is not None:
         sql_table_animal = (
@@ -23,66 +26,53 @@ def create_all_tables(connection):
             "IDANIMAL INT PRIMARY KEY AUTO_INCREMENT NOT NULL,"
             "NOME_ANIMAL VARCHAR(100) NOT NULL,"
             "SEXO ENUM('M', 'F') NOT NULL,"
-            "RACA ENUM('GATO', 'CACHORRO') NOT NULL,"
-            "PORTE ENUM('PEQUENO', 'MEDIA', 'GRANDE') NOT NULL,"
-            "OBS VARCHAR(200)"
+            "ESPECIE ENUM('GATO', 'CACHORRO') NOT NULL,"
+            "PORTE ENUM('PEQUENO', 'MEDIO', 'GRANDE') NOT NULL,"
+            "CAUSA VARCHAR(255) NOT NULL,"
+            "DELETED BOOLEAN NOT NULL DEFAULT FALSE,"
+            "OBS VARCHAR(255)"
             ")"
         )
-        sql_table_veterinario = (
-            "CREATE TABLE IF NOT EXISTS VETERINARIO("
-            "IDVETERINARIO INT PRIMARY KEY AUTO_INCREMENT,"
-            "NOME_VET VARCHAR(100)"
+
+        sql_table_clinica = (
+            "CREATE TABLE IF NOT EXISTS CLINICA("
+            "IDCLINICA INT PRIMARY KEY AUTO_INCREMENT,"
+            "DELETED BOOLEAN NOT NULL DEFAULT FALSE,"
+            "NOME_CLINICA VARCHAR(100)"
             ")"
         )
+
         sql_table_endereco = (
             "CREATE TABLE IF NOT EXISTS ENDERECO("
             "IDENDERECO INT PRIMARY KEY AUTO_INCREMENT,"
-            "CEP VARCHAR(9),"
-            "RUA VARCHAR(100),"
-            "NUMERO VARCHAR(6),"
-            "BAIRRO VARCHAR(100),"
-            "CIDADE VARCHAR(100),"
-            "ESTADO CHAR(2)"
+            "RUA VARCHAR(255) NOT NULL,"
+            "BAIRRO VARCHAR(100)"
             ")"
         )
-        sql_table_telefone = (
-            "CREATE TABLE IF NOT EXISTS TELEFONE("
-            "IDTELEFONE INT PRIMARY KEY AUTO_INCREMENT,"
-            "NUMERO VARCHAR(17)"
-            ")"
-        )
-        sql_table_tutor = (
-            "CREATE TABLE IF NOT EXISTS TUTOR("
-            "IDTUTOR INT PRIMARY KEY AUTO_INCREMENT,"
-            "NOME_TUTOR VARCHAR(100),"
-            "ENDERECO_ID INT,"
-            "FOREIGN KEY(ENDERECO_ID) REFERENCES ENDERECO(IDENDERECO)"
-            ")"
-        )
+
         sql_table_atendimento = (
             "CREATE TABLE IF NOT EXISTS ATENDIMENTO("
             "IDATENDIMENTO INT PRIMARY KEY AUTO_INCREMENT NOT NULL,"
             "DATA DATE NOT NULL,"
-            "HORA TIME NOT NULL,"
+            "DELETED BOOLEAN NOT NULL DEFAULT FALSE,"
             "ANIMAL_ID INT NOT NULL,"
-            "VETERINARIO_ID INT NOT NULL,"
+            "CLINICA_ID INT NOT NULL,"
             "ENDERECO_ID INT NOT NULL,"
-            "TUTOR_ID INT NOT NULL,"
-            "TELEFONE_ID INT NOT NULL,"
             "FOREIGN KEY (ANIMAL_ID) REFERENCES ANIMAL(IDANIMAL),"
-            "FOREIGN KEY (VETERINARIO_ID) REFERENCES VETERINARIO(IDVETERINARIO),"
-            "FOREIGN KEY (ENDERECO_ID) REFERENCES ENDERECO(IDENDERECO),"
-            "FOREIGN KEY (TUTOR_ID) REFERENCES TUTOR(IDTUTOR),"
-            "FOREIGN KEY (TELEFONE_ID) REFERENCES TELEFONE(IDTELEFONE)"
+            "FOREIGN KEY (CLINICA_ID) REFERENCES CLINICA(IDCLINICA),"
+            "FOREIGN KEY (ENDERECO_ID) REFERENCES ENDERECO(IDENDERECO)"
             ")"
         )
+
+
+
         create_table(connection, sql_table_animal)
-        create_table(connection, sql_table_veterinario)
+        create_table(connection, sql_table_clinica)
         create_table(connection, sql_table_endereco)
-        create_table(connection, sql_table_telefone)
-        create_table(connection, sql_table_tutor)
         create_table(connection, sql_table_atendimento)
         print("Tabelas criadas com sucesso!")
+
+
 def create_table(connection, sql):
     try:
         cursor = connection.cursor()
@@ -93,6 +83,8 @@ def create_table(connection, sql):
     finally:
         if cursor:
             cursor.close()
+
+
 def insert_into_table(connection, sql, params):
     try:
         cursor = connection.cursor()
@@ -105,6 +97,8 @@ def insert_into_table(connection, sql, params):
     finally:
         if cursor:
             cursor.close()
+
+
 def select_table(connection, query):
     try:
         with connection.cursor() as cursor:
